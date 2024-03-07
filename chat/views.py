46a4ed204f1a4cf0fa011fabe793bd58cfca5fa2
Igor Stoleski <72672441+IgorStoleski@users.noreply.github.com>
2024-03-07 19:41:02 +0100

    add some logic

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,57 @@
 from django.shortcuts import render
+from .models import Message, Chat
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+@login_required(login_url="/login/")
+def index(request):
+    if request.method == "POST":
+        print("received data " + request.POST["textmessage"])
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(
+            text=request.POST["textmessage"],
+            chat=myChat,
+            author=request.user,
+            receiver=request.user,
+        )
+    chatMessages = Message.objects.filter(chat__id=1)
+    return render(request, "chat/index.html", {"messages": chatMessages})
+
+
+def login_view(request):
+    redirect = request.GET.get("next")
+    if request.method == "POST":
+        user = authenticate(
+            username=request.POST.get("username"), password=request.POST.get("password")
+        )
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(request.POST.get("redirect"))
+        else:
+            return render(
+                request,
+                "auth/login.html",
+                {"wrongPassword": True, "redirect": redirect},
+            )
+    return render(request, "auth/login.html", {"redirect": redirect})
+
+
+def register_view(request):
+    redirect = request.GET.get("next")
+    password = request.POST.get("password")
+    checkPassword = request.POST.get("checkPassword")
+    if request.method == "POST":
+        user = User.objects.create_user(
+            username=request.POST.get("username"),
+            password=request.POST.get("password")
+        )
+        if password == checkPassword:
+            user.save()
+            return HttpResponseRedirect(request.POST.get("redirect"))
+        else:
+            return render(request, "auth/login.html", {"redirect": redirect})
+
+    return render(request, "register/register.html", {"redirect": redirect})
--- django_chat_app/urls.py
+++ django_chat_app/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
+from chat.views import index, login_view, register_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('chat/', index),
+    path('login/', login_view),
+    path('register/', register_view),
 ]
